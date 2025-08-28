27db174e6e4d6187f7f17b41d96b0b212f0c4fe3
utkrshydv <utkarsh4socials@gmail.com>
2025-08-28 05:36:35 +0530

    complete

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -6,14 +6,14 @@ const playButton = document.querySelector(".translate-button")
 const textInput = document.querySelector("#inputText");
 const outText = document.querySelector("#outputText")
 
-textInput.addEventListener("focus", function handler(){
-  textInput.value ="";
+textInput.addEventListener("focus", function handler() {
+  textInput.value = "";
   textInput.removeEventListener("focus", handler);
 })
 
 
 const languages = [
-  { code: 'hi', name: 'Hindi'},
+  { code: 'hi', name: 'Hindi' },
   { code: 'en', name: 'English' },
   { code: 'es', name: 'Spanish' },
   { code: 'fr', name: 'French' },
@@ -33,25 +33,25 @@ const googleVoices = [
 ]
 
 
-  languageSelect1.innerHTML = languages.map((language) => {
-    return `<option value=${language.code}>${language.name}</option>`
-  }).join("")
+languageSelect1.innerHTML = languages.map((language) => {
+  return `<option value=${language.code}>${language.name}</option>`
+}).join("")
 
-   languageSelect2.innerHTML = languages.map((language) => {
-    return `<option value=${language.code}>${language.name}</option>`
-  }).join("")
+languageSelect2.innerHTML = languages.map((language) => {
+  return `<option value=${language.code}>${language.name}</option>`
+}).join("")
 
 
 let voices = [];
 
-function loadVoices(){
+function loadVoices() {
   voices = speechSynthesis.getVoices();
 
   voiceSelect.innerHTML = googleVoices.map((voice, index) => {
     return `<option value=${index}>${voice.name} </option>`
   }).join("");
 
- 
+
 }
 
 loadVoices();
@@ -68,34 +68,40 @@ playButton.addEventListener('click', async () => {
 
   outText.value = "translating...."
 
-  try{
+  try {
     const response = await fetch(`http://localhost:5000/translate?sl=${sourceLang}&dl=${targetLang}&text=${encodeURIComponent(text)}`);
     const data = await response.json();
     console.log(data);
 
-  
-   
+
+
 
     const translatedText = data["destination-text"];
     outText.value = translatedText;
 
-    
+
 
     const utterance = new SpeechSynthesisUtterance(translatedText);
 
-    const googleVoice = googleVoices[voiceSelect.value];
+    const googleVoice = googleVoices[Number(voiceSelect.value)];
     const realVoice = voices.find(v => v.name.includes(googleVoice.name));
 
     if (realVoice) {
       utterance.voice = realVoice;
     }
 
+    speechSynthesis.speak(utterance);
 
-     speechSynthesis.speak(utterance);
-   
-  } catch(err){
+  } catch (err) {
     console.log("Translation error: ", err);
     alert("Failed to translate text");
   }
 
+})
+
+textInput.addEventListener("keydown", (event)=>{
+  if(event.key === "Enter"){
+    event.preventDefault();
+    playButton.click();
+  }
 })
